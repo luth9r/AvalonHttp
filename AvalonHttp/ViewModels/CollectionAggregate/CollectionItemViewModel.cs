00b330252a105a762cc601b384b6e7e21a1cc694
Dmytro Zhadan <dzadan73@gmail.com>
2026-02-01 00:55:20 +0100

    feat: add `CollectionItemViewModel` and `RequestItemViewModel` to manage API collections and requests

---
+++ AvalonHttp/ViewModels/CollectionAggregate/CollectionItemViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using AvalonHttp.Models.CollectionAggregate;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ApiRequest = AvalonHttp.Models.CollectionAggregate.ApiRequest;
+
+namespace AvalonHttp.ViewModels.CollectionAggregate;
+
+public partial class CollectionItemViewModel : ObservableObject
+{
+    public readonly CollectionsViewModel _parent;
+    
+    public CollectionsViewModel Parent => _parent;
+
+    [ObservableProperty]
+    private Guid _id;
+
+    [ObservableProperty]
+    private string _name;
+
+    [ObservableProperty]
+    private string _description;
+
+    [ObservableProperty]
+    private bool _isExpanded = true;
+
+    [ObservableProperty]
+    private bool _isEditing = false;
+
+    [ObservableProperty]
+    private ObservableCollection<RequestItemViewModel> _requests = new();
+
+    public CollectionItemViewModel(ApiCollection collection, CollectionsViewModel parent)
+    {
+        _parent = parent;
+        _id = collection.Id;
+        _name = collection.Name;
+        _description = collection.Description;
+
+        foreach (var request in collection.Requests)
+        {
+            Requests.Add(new RequestItemViewModel(request, this));
+        }
+    }
+
+    [RelayCommand]
+    private void ToggleExpand()
+    {
+        IsExpanded = !IsExpanded;
+    }
+
+    [RelayCommand]
+    private void StartRename()
+    {
+        IsEditing = true;
+    }
+
+    [RelayCommand]
+    private async Task FinishRename()
+    {
+        IsEditing = false;
+        await _parent.SaveCollectionCommand.ExecuteAsync(this);
+    }
+
+    [RelayCommand]
+    private async Task AddRequest()
+    {
+        var request = new ApiRequest
+        {
+            Name = "New Request",
+            Url = "https://",
+            MethodString = "GET"
+        };
+
+        var viewModel = new RequestItemViewModel(request, this);
+        Requests.Add(viewModel);
+        await _parent.SaveCollectionCommand.ExecuteAsync(this);
+    }
+
+    [RelayCommand]
+    private async Task DeleteRequest(RequestItemViewModel request)
+    {
+        Requests.Remove(request);
+        await _parent.SaveCollectionCommand.ExecuteAsync(this);
+    }
+
+    [RelayCommand]
+    private async Task DuplicateRequest(RequestItemViewModel request)
+    {
+        var newRequest = request.ToModel();
+        newRequest.Name = $"{request.Name} (Copy)";
+
+        var viewModel = new RequestItemViewModel(newRequest, this);
+        var index = Requests.IndexOf(request);
+        Requests.Insert(index + 1, viewModel);
+        
+        await _parent.SaveCollectionCommand.ExecuteAsync(this);
+    }
+
+    public ApiCollection ToModel()
+    {
+        return new ApiCollection
+        {
+            Id = Id,
+            Name = Name,
+            Description = Description,
+            Requests = new ObservableCollection<ApiRequest>(
+                Enumerable.Select<RequestItemViewModel, ApiRequest>(Requests, r => r.ToModel()))
+        };
+    }
+}
---
+++ AvalonHttp/ViewModels/CollectionAggregate/RequestItemViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using AvalonHttp.Messages;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using ApiRequest = AvalonHttp.Models.CollectionAggregate.ApiRequest;
+
+namespace AvalonHttp.ViewModels.CollectionAggregate;
+
+public partial class RequestItemViewModel : ObservableObject
+{
+    public CollectionItemViewModel Parent { get; set; }
+    
+    [ObservableProperty]
+    private string _name;
+
+    [ObservableProperty]
+    private string _url;
+
+    [ObservableProperty]
+    private string _method;
+
+    [ObservableProperty]
+    private string _body;
+
+    [ObservableProperty]
+    private bool _isEditing = false;
+    
+    [ObservableProperty]
+    private bool _isDirty;
+
+    [ObservableProperty]
+    private bool _isSelected = false;
+    
+    public Guid Id => _originalRequest.Id;
+
+    private readonly ApiRequest _originalRequest;
+
+    public RequestItemViewModel(ApiRequest request, CollectionItemViewModel parent)
+    {
+        Parent = parent;
+        _originalRequest = request;
+        
+        _name = request.Name;
+        _url = request.Url;
+        _method = request.MethodString;
+        _body = request.Body;
+        
+        _originalRequest.PropertyChanged += OnModelPropertyChanged;
+    }
+    
+    private void OnModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(ApiRequest.Name))
+        {
+            Name = _originalRequest.Name;
+        }
+        else if (e.PropertyName == nameof(ApiRequest.MethodString))
+        {
+            Method = _originalRequest.MethodString;
+        }
+    }
+    
+    partial void OnNameChanged(string value)
+    {
+        if (_originalRequest.Name != value)
+        {
+            _originalRequest.Name = value;
+        }
+    }
+
+    [RelayCommand]
+    private void Select()
+    {
+        Parent.Parent.SelectRequest(this);
+    }
+
+    [RelayCommand]
+    private void StartRename()
+    {
+        IsEditing = true;
+    }
+
+    [RelayCommand]
+    private async Task FinishRename()
+    {
+        IsEditing = false;
+    }
+
+    [RelayCommand]
+    private void Delete()
+    {
+        WeakReferenceMessenger.Default.Send(new ConfirmMessage(
+            "Delete Request?",
+            $"Are you sure you want to delete '{Name}'?",
+            async () =>
+            {
+                await Parent.DeleteRequestCommand.ExecuteAsync(this);
+            }
+        ));
+    }
+
+    [RelayCommand]
+    private async Task Duplicate()
+    {
+        await Parent.DuplicateRequestCommand.ExecuteAsync(this);
+    }
+
+    [RelayCommand]
+    private void MoveToCollection(CollectionItemViewModel targetCollection)
+    {
+        Parent.Requests.Remove(this);
+        
+        var oldParent = Parent;
+        Parent = targetCollection;
+        
+        targetCollection.Requests.Add(this);
+
+        _ = oldParent.Parent.SaveCollectionCommand.ExecuteAsync(oldParent);
+        _ = Parent.Parent.SaveCollectionCommand.ExecuteAsync(Parent);
+    }
+    
+    public ApiRequest ToModel()
+    {
+        return _originalRequest;
+    }
+}
