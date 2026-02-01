d9ffb47c465660788681b4b2db0406ca9e7dd697
Dmytro Zhadan <dzadan73@gmail.com>
2026-02-01 16:09:31 +0100

    refactor: remove unused `KeyValueData` model from `CollectionAggregate`

--- AvalonHttp/Models/CollectionAggregate/KeyValueData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AvalonHttp.Models.CollectionAggregate;
-
-public class KeyValueData
-{
-    public string Key { get; set; } = "";
-    public string Value { get; set; } = "";
-    public bool IsEnabled { get; set; } = true;
-}
